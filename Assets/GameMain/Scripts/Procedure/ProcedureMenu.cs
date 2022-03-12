9303cd576a8bb3f440177cd4dfad82246082003e
CheapMiao <1822175458@qq.com>
2022-03-12 18:08:46 +0800

    树大招风
    已搭建 GameFramework

---
+++ Assets/GameMain/Scripts/Procedure/ProcedureMenu.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework.Event;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace OaksMayFall
+{
+    public class ProcedureMenu : ProcedureBase
+    {
+        private bool m_StartGame = false;
+        private MenuForm m_MenuForm = null;
+
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void StartGame()
+        {
+            m_StartGame = true;
+        }
+    }
+}
